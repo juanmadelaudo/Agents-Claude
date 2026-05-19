8f8f3e9966dacec5e1a5bee16bd83e29ddf44631
juanmadelaudo <juan.manuel.delaudo@accenture.com>
2026-05-19 12:15:26 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Si tu repo en GitHub se llama "qa-analyzer", el base debe ser "/qa-analyzer/"
-// Cambialo al nombre exacto de tu repositorio GitHub
 export default defineConfig({
   plugins: [react()],
   base: '/qa-analyzer/',
